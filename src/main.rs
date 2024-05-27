23e2f369e5e86f5350e4aff679be8f8b2db026db
Abirbhav G <abirbhav@thelogisticsnow.com>
2024-05-27 11:07:43 +0530

    feat: start repl and discard unknown commands

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    // REPL
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        input = input.trim().to_string();
+        match input {
+            _ => eprintln!("{}: command not found", input)
+        }
+    }
 }
